16d83e3e0db833bdf107c3ae7f5ba05c14bfe3e8
Hannitavrrr <160497556+Hannitavrrr@users.noreply.github.com>
2024-07-18 06:46:28 -0600

    Update CardGame.java

--- CardGame.java
+++ CardGame.java
@@ -1,28 +1,31 @@
-package university.jala.institutional.cartas;
-
 import java.util.*;
 
+
 public class CardGame {
-    public static void main(String[] args) {
-        Deck deck = new Deck();
-        Random random = new Random();
+   public static void main(String[] args) {
+       Deck deck = new Deck();
+       Random random = new Random();
 
-        // Simular una jugada
-        Card computerCard = deck.dealCard();
-        Card playerCard = deck.dealCard();
 
-        // Imprimir las cartas obtenidas
-        System.out.println("Computer: " + computerCard);
-        System.out.println("Player:  " + playerCard);
+       // Simular una jugada
+       Card computerCard = deck.dealCard();
+       Card playerCard = deck.dealCard();
 
-        // Comparar las cartas
-        int compareResult = playerCard.getValue().ordinal() - computerCard.getValue().ordinal();
-        if (compareResult > 0) {
-            System.out.println("Ganaste!");
-        } else if (compareResult < 0) {
-            System.out.println("Perdiste.");
-        } else {
-            System.out.println("Empate.");
-        }
-    }
+
+       // Imprimir las cartas 
+       System.out.println("Computer: " + computerCard);
+       System.out.println("Player:  " + playerCard);
+
+
+       // Comparar las cartas
+       int compareResult = playerCard.getValue().ordinal() - computerCard.getValue().ordinal();
+       if (compareResult > 0) {
+           System.out.println("Ganaste!");
+       } else if (compareResult < 0) {
+           System.out.println("Perdiste.");
+       } else {
+           System.out.println("Empate.");
+       }
+   }
 }
+
